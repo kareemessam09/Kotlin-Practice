65be8c9bca9df1e3dcd7a323d15410f73de15335
kareemessam09 <kareemessam.me@gmail.com>
2024-04-03 16:12:08 +0200

    fun1

--- main.kt
+++ main.kt
@@ -1,3 +1,22 @@
-fun main(args: Array<String>) {
-    println("Hello, World!")
+import java.util.Calendar
+
+fun main() {
+    println("Hello, Kemo!")
+    dayOfWeek()
+}
+
+fun dayOfWeek() {
+    println("what is the day today?")
+    val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    println( when(day){
+        1 -> "sunday"
+        2 -> "monday"
+        3 -> "tuesday"
+        4 -> "wednesday"
+        5 -> "thursday"
+        6 -> "friday"
+        7 -> "saturday"
+        else -> "time has stopped"
+    })
+
 }
